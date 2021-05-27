290ebe34d79dc7faff270effd75559f5a4c85563
manav260520 <84763615+manav260520@users.noreply.github.com>
2021-05-27 22:19:10 +0530

    Add files via upload

---
+++ Day 2/example3.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = 4;
+switch ($a) {
+    case 1:
+        echo "one";
+        break;
+
+    case 2:
+            echo "two";
+            break;
+
+    case 3:
+        echo "three";
+        break;
+    
+    default:
+         echo "please select 1-3";
+        break;
+}
