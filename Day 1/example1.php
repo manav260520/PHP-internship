ce1faaa7150d2bb1a404012b679b35ecb4467a96
manav260520 <84763615+manav260520@users.noreply.github.com>
2021-05-27 22:17:52 +0530

    Add files via upload

---
+++ Day 1/example1.php
@@ -0,0 +1,9 @@
+<?php
+$a = 10;
+$b = 20;
+echo "A value is".$a;
+echo "<br/>B value is ".$b;
+echo "<br/> A value is".$a."<br/>B value is".$b;
+echo "<br/> A value is $a<br/>b value is $b";
+echo '<br/>A value is $a<br/>B value is.%b';
+echo "<br/>A value is$a<br/>B value is$b sum is".($a+$b);
--- Day 1/welcome.php
+++ Day 1/welcome.php
@@ -1,1 +1,12 @@
-
+<?php
+echo "welcome to php";
+echo "<h1> I am H1 in php";
+?>
+<html>
+    <body>
+        welcome to html code 
+        <?php
+        echo "I am php in html";
+        ?>
+    </body>
+</html>
