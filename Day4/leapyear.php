91b024a82b43d2dd2c17eceac493cbea2ee5b04f
manav260520 <84763615+manav260520@users.noreply.github.com>
2021-05-31 17:08:23 +0530

    Add files via upload

---
+++ Day4/leapyear.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Leap Year Demo</title>
+</head>
+<body>
+<?php
+$year=2016;
+echo "</br>";
+if($year%4==0)
+{
+    echo "$year is a leap year.";
+}
+else
+{
+    echo "$year is not a leap year.";
+}
+?>
+</body>
+</html>
