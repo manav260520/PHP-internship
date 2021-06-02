4fa257145daada5d659ab116e53103ba0b6d653d
manav260520 <84763615+manav260520@users.noreply.github.com>
2021-06-02 13:34:42 +0530

    Add files via upload

--- Day4/Mywebsite/master.php
+++ Day4/Mywebsite/master.php
@@ -22,11 +22,10 @@ include './themepart/logo.php';
             <div><img src="images/slider.jpeg" width="920" height="700" alt="" /></div>
 		<div id="content">
 			<div class="post">
-				<h2 class="title"><a href="#">Welcome   to   My  Website</a></h2>
+				<h2 class="title"><a href="#">Welcome   to   Webpage</a></h2>
 				<p class="meta"><span class="date">June 01, 2021</span><span class="posted">Posted by <a href="#">Manav Gandhi</a></span></p>
 				<div style="clear: both;">&nbsp;</div>
 				<div class="entry">
-                                    <h3>Heyy Everyone</h3><h6>My name is manav gandhi</h6>
 					<p>This is <strong>Webpage</strong>, a free, fully standards-compliant CSS template designed by <a href="http://www.freecsstemplates.org">FCT</a>.  The photo used in this template is from <a href="http://fotogrph.com/">Fotogrph</a>.  This free template is released under a <a href="http://creativecommons.org/licenses/by/3/">Creative Commons Attributions 3.0</a> license, so you’re pretty much free to do whatever you want with it (even use it commercially) provided you keep the links in the footer intact. Aside from that, have fun with it :)</p>
 					<p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum ipsum. Proin imperdiet est. Phasellus dapibus semper urna. Pellentesque ornare, orci in felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem.</p>
 					<p class="links"><a href="#">Read More</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href="#">Comments</a></p>
