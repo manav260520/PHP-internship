7c239ab548a7cb30607730d53ed1874fec4cf00c
manav260520 <84763615+manav260520@users.noreply.github.com>
2021-06-03 12:19:35 +0530

    Add files via upload

--- Day6/connection.php
+++ Day6/connection.php
@@ -1,1 +1,15 @@
+<?php
+$host = "localhost";
+$username = "root";
+$passwd = "";
+$dbname = "db_internship";
+$connection = mysqli_connect($host, $username, $passwd,$dbname);
 
+$q = mysqli_query($connection, 
+        "insert into tbl_user(user_name,user_gender,user_mobile) values('Manav','Male','123')") 
+        or die("Error". mysqli_error($connection));
+
+if($q)
+{
+    echo "<script>alert('Record Added');</script>";
+}
